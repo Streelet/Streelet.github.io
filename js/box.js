21a51a5b92f54d093c56f96b3354388049644489
Streelet <111197047+Streelet@users.noreply.github.com>
2023-06-25 16:45:36 -0600

    Add files via upload

--- js/box.js
+++ js/box.js
@@ -1,10 +1,23 @@
+var requestId;
+var startTime;
 let showTimeoutId;
+let hideTimeoutId;
 let isHKeyPressed = false;
+let active=false;
+let hProgressBar = document.getElementById('h-indicator');
+let progressIntervalId;
+let progress = 0;
+
 
 document.addEventListener('keydown', function(event) {
-  if (event.key === 'h' && !isHKeyPressed) {
+  if (event.key === 'h' && !isHKeyPressed && active==false) {
     isHKeyPressed = true;
-    timeoutId = setTimeout(showBox, 1000);
+    showTimeoutId = setTimeout(showBox, 1000);
+    
+    
+  }
+  else if (event.key === 'h' && !isHKeyPressed && active==true){
+    hideTimeoutId=setTimeout(hideBox,1000)
     
   }
 });
@@ -13,17 +26,99 @@ document.addEventListener('keyup', function(event) {
   if (event.key === 'h' && isHKeyPressed) {
     clearTimeout(showTimeoutId);
     isHKeyPressed = false;
-    showTimeoutId = setTimeout(hideBox, 5000);
+    showTimeoutId = setTimeout(hideBox, 10000);
     
   }
 });
 
 function showBox() {
   const hiddenBox = document.getElementById('hiddenBox');
-  hiddenBox.style.bottom = '0';
+ hiddenBox.style.bottom = '0';
+  active=true;
+  resetProgress();
 }
 
 function hideBox() {
   const hiddenBox = document.getElementById('hiddenBox');
   hiddenBox.style.bottom = '-100%';
+  active=false;
+  clearInterval(hideTimeoutId);
 }
+
+//PROGRESS
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'h' && !progressIntervalId) {
+    progressIntervalId = setInterval(updateProgress, 100);
+  }
+});
+
+document.addEventListener('keyup', function(event) {
+  if (event.key === 'h' && progressIntervalId) {
+    clearInterval(progressIntervalId);
+    progressIntervalId = null;
+    resetProgress();
+  }
+});
+
+function updateProgress() {
+  progress += 10;
+  if (progress > 100) {
+    progress = 100;
+    showBox();
+  }
+  hProgressBar.style.borderColor = `rgba(0, 128, 0, ${progress / 100})`;
+}
+
+function resetProgress() {
+  progress = 0;
+  hProgressBar.style.borderColor = '#ccc';
+}
+
+//Appareance
+
+var requestId;
+    var startTime;
+
+    function fillBorder(timestamp) {
+      var filling = document.getElementById('filling');
+      var duration = 3000; // Duración total del relleno en milisegundos
+      var maxWidth = 100; // Ancho máximo del relleno en porcentaje
+
+      if (!startTime) {
+        startTime = timestamp;
+      }
+
+      var elapsedTime = timestamp - startTime;
+      var progress = elapsedTime / duration;
+      var currentWidth = Math.min(progress * maxWidth, maxWidth);
+
+      filling.style.width = currentWidth + '%';
+
+      if (progress < 1) {
+        requestId = requestAnimationFrame(fillBorder);
+      }
+    }
+
+    function resetBorder() {
+      cancelAnimationFrame(requestId);
+      var filling = document.getElementById('filling');
+      filling.style.width = '0';
+      startTime = null;
+    }
+
+    document.addEventListener('keydown', function(event) {
+      if (event.key === 'h') {
+        requestId = requestAnimationFrame(fillBorder);
+      }
+    });
+
+    document.addEventListener('keyup', function(event) {
+      if (event.key === 'h') {
+        resetBorder();
+      }
+    });
+
+
+
+
--- js/music-player.js
+++ js/music-player.js
@@ -1,19 +1,95 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var audio = document.getElementById("mi-audio");
-    var playButton = document.getElementById("play-button");
-    var isPlaying = false;
-  
-    playButton.addEventListener("click", function() {
-      if (isPlaying) {
-        // Pausar la música
-        audio.pause();
-        playButton.innerHTML = '<i class="fas fa-play"></i>';
-      } else {
-        // Reproducir la música
-        audio.play();
-        playButton.innerHTML = '<i class="fas fa-pause"></i>';
-      }
-      isPlaying = !isPlaying; // Alternar el estado entre reproducir y pausar
+  var playButton = document.getElementById("play-button");
+  var isPlaying = false;
+  var songs = document.querySelectorAll(".songs li");
+  var currentSongIndex = 0;
+  var progressBar = document.getElementById("progress");
+  var currentTimeLabel = document.getElementById("current-time");
+  var audio = new Audio();
+
+  function playSong() {
+    audio.play();
+    playButton.innerHTML = '<i class="fas fa-pause"></i>';
+    isPlaying = true;
+  }
+
+  function pauseSong() {
+    audio.pause();
+    playButton.innerHTML = '<i class="fas fa-play"></i>';
+    isPlaying = false;
+  }
+
+  function playNextSong() {
+    currentSongIndex = (currentSongIndex + 1) % songs.length;
+    loadSong();
+    playSong();
+  }
+
+  function playPreviousSong() {
+    currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+    loadSong();
+    playSong();
+  }
+
+  function loadSong() {
+    var song = songs[currentSongIndex];
+    var songTitle = song.querySelector("h6").textContent;
+    var songInfo = song.querySelector("p").textContent;
+    var actualSongTitle = document.querySelector("#actual-song h6");
+    var actualSongInfo = document.querySelector("#actual-song p");
+
+    actualSongTitle.textContent = songTitle;
+    actualSongInfo.textContent = songInfo;
+
+    song.querySelector("a").addEventListener("click", function(event) {
+      event.preventDefault();
     });
-  });
+
+    audio.src = song.querySelector("a").href;
+  }
+
+  function handleSongEnd() {
+    playNextSong();
+  }
+
+  function updateProgressBar() {
+    var progress = (audio.currentTime / audio.duration) * 100;
+    if (progressBar) {
+      progressBar.style.width = progress + "%";
+    }
+    var currentTime = formatTime(audio.currentTime);
+    if (currentTimeLabel) {
+      currentTimeLabel.textContent = currentTime;
+    }
+  }
   
+
+  function formatTime(time) {
+    var minutes = Math.floor(time / 60);
+    var seconds = Math.floor(time % 60);
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    return minutes + ":" + seconds;
+  }
+
+  playButton.addEventListener("click", function() {
+    if (isPlaying) {
+      pauseSong();
+    } else {
+      playSong();
+    }
+  });
+
+  document.getElementById("next-button").addEventListener("click", function() {
+    playNextSong();
+  });
+
+  document.getElementById("previous-button").addEventListener("click", function() {
+    playPreviousSong();
+  });
+
+  audio.addEventListener("ended", handleSongEnd);
+  audio.addEventListener("timeupdate", updateProgressBar);
+
+  loadSong();
+});
+
